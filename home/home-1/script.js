3933ea02f58038b8104f6086834010f9d4b6cd06
TurtleLearningCoding <22h1120071@ut.edu.vn>
2025-03-23 00:13:13 +0700

    Update

---
+++ home/home-1/script.js
@@ -0,0 +1,11 @@
+
+/*Dribble Design credits
+Card #1: https://dribbble.com/shots/2042501-Potato-landing-page
+
+Card #2: https://cdn.dribbble.com/users/4605/screenshots/1757843/snap.png
+
+Card #3: https://cdn.dribbble.com/users/33073/screenshots/2425824/800.png
+
+Card #4: https://cdn.dribbble.com/users/25514/screenshots/2595669/payment_plans_illustrations_ramotion.png
+
+*/
